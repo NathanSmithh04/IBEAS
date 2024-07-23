f2ec72ca63e93b926298e542516fcd0a39893391
Nathan Smith <nts230001@utdallas.edu>
2024-07-22 21:20:31 -0500

    changed db url

--- backend/src/app.py
+++ backend/src/app.py
@@ -18,7 +18,7 @@ scheduler = BackgroundScheduler()
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
