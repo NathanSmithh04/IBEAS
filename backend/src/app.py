a84d6e2989f50a042218b89b3e74336fa8a8d4dc
Nathan Smith <nts230001@utdallas.edu>
2024-08-12 20:25:05 -0500

    Procfile

--- backend/src/app.py
+++ backend/src/app.py
@@ -679,8 +679,6 @@ def init_scheduler():
         if email.interval:
             schedule_email_interval(email)
         
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        init_scheduler()
-    app.run(host='0.0.0.0', port=5000)
+with app.app_context():
+    db.create_all()
+    init_scheduler()
